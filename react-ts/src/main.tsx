2c1fc0cf8ef0628ed46574cb005a598a94c8704e
yuki-0211 <yoshizumi.kf@om.asahi-kasei.co.jp>
2023-01-10 19:14:16 +0900

    settings: add absolute import setting

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import './index.css';
+import App from 'App.js';
+import 'index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
 	<React.StrictMode>
---
+++ react-ts/src/pages/BasePage.tsx
@@ -0,0 +1,8 @@
+import {AppBar, ThemeProvider} from '@mui/material';
+import BaseHeader from '../componets/BaseHeader';
+
+const BasePage: React.FC = () => {
+	return <BaseHeader />;
+};
+
+export default BasePage;
